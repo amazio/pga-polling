773ffe41b200a03fd1caaca116bcd92ac4374d63
amazio <email@jim-clark.com>
2019-11-29 13:31:38 -0800

    Update index view

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,14 @@
 var express = require('express');
 var router = express.Router();
+var Settings = require('../models/settings');
+var Tournament = require('../models/tournament');
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index');
+router.get('/', async function(req, res, next) {
+  res.render('index', {
+    settings: await Settings.findOne({}),
+    tourney: await Tournament.findOne({}).sort('-createdAt')
+  });
 });
 
 module.exports = router;
