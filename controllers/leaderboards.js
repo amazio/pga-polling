3e6760cc116d596608fe259687b44edecb7213a6
amazio <email@jim-clark.com>
2020-01-28 11:40:52 -0800

    Return doc instead of array of doc

--- controllers/leaderboards.js
+++ controllers/leaderboards.js
@@ -6,7 +6,7 @@ module.exports = {
 
 async function current(req, res) {
   try {
-    const tourney = await Tournament.find().sort({updatedAt: -1}).limit(1);
+    const tourney = await Tournament.findOne().sort({updatedAt: -1});
     return res.json(tourney);
   } catch (e) {
     return res.status(400).json(e);
